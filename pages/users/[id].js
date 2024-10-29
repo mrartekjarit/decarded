42c8bfcd1a65de3bb8cf09b809083728c0cf3d39
Artekя <ajaritus@gmail.com>
2024-10-29 17:47:30 +0100

    id

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,13 +1,28 @@
 import { useRouter } from 'next/router';
 
-const User = () => {
-  const router = useRouter();
-  const { id } = router.query; // Получение параметра из URL
+export async function getStaticPaths() {
+  const paths = [
+    { params: { id: '1' } },
+    { params: { id: '2' } },
+    { params: { id: '3' } },
+  ];
+  return { paths, fallback: false };
+}
 
+export async function getStaticProps({ params }) {
+  const { id } = params;
+  return {
+    props: {
+      user: { id, name: `User ${id}` }, // Пример данных
+    },
+  };
+}
+
+const User = ({ user }) => {
   return (
     <div>
-      <h1>User ID: {id}</h1>
-      <p>This is the user page.</p>
+      <h1>User ID: {user.id}</h1>
+      <p>Name: {user.name}</p>
     </div>
   );
 };
