92c112cab47f24b61eb1af42b4b775a35232e067
Artekя <ajaritus@gmail.com>
2024-10-29 17:31:52 +0100

    about

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,8 @@
 export default function About() {
-    return (
-      <div>
-        <h1>About Us</h1>
-        <p>This is the about page</p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the about page for our Next.js app.</p>
+    </div>
+  );
+}
