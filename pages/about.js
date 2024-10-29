bc20df0fd1f229dcd5b4b6d7d8021d74b3ddd3a0
Artekя <ajaritus@gmail.com>
2024-10-29 17:35:21 +0100

    Add about page with styles

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,9 @@
+import styles from '../styles/About.module.css';
+
 export default function About() {
   return (
-    <div>
-      <h1>About Us</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>About Us</h1>
       <p>This is the about page for our Next.js app.</p>
     </div>
   );
